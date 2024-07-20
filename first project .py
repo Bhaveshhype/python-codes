9ad24a6e6a3ef8b34467073b9683eb0577a95993
Bhavesh Patil <123001201+Bhaveshhype@users.noreply.github.com>
2024-07-20 14:22:26 +0530

    Update first project .py

--- first project .py
+++ first project .py
@@ -2,7 +2,6 @@ num1 = float(input('Enter your first number: '))
 Operator = input('Enter operator: ')
 num2 = float(input('Enter your second number: '))
 
-# if Operator is (+ | - | * | /) then  print out number 1 (+ | - | * | /) number 2
 if Operator == '+':
     print(num1 + num2)
 elif Operator == '-':
@@ -11,7 +10,5 @@ elif Operator == '/':
     print(num1 / num2)
 elif Operator == '*':
     print(num1 * num2)
-
-# if the user didn't put an operator
 else:
     print('Not a valid operator')

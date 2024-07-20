a4cafafeb44ffd72a2ffec7049c71f53f862f00d
Bhavesh Patil <123001201+Bhaveshhype@users.noreply.github.com>
2024-07-20 14:24:50 +0530

    Delete First Program.py

--- First Program.py
+++
@@ -1,7 +0,0 @@
-age=int(input("enter the age"))
-if(age>=18):
-    print("you are eligible to vote")
-else:
-    yrs=18-age
-    print("you have to wait for another ", yrs, " years to cast your vote")
-
